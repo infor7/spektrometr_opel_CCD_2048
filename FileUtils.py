37270f66098a8bd56e91e07cca6ab07181b474d9
Paweł Zieliński <xziele@gmail.com>
2018-12-03 09:39:55 +0100

    change private to public

--- FileUtils.py
+++ FileUtils.py
@@ -1,9 +1,9 @@
 class FileUtils:
-    def _read_data(self, filename):
+    def read_data(self, filename):
         with open(filename, 'r') as fh:
             return [int(x) for x in fh.readlines()]
 
 
 if __name__ == "__main__":
     data = FileUtils()
-    print(data._read_data("data_from_spec.txt"))
+    print(data.read_data("data_from_spec.txt"))
