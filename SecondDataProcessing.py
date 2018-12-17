9320656d01a1899300cd02a33e4011967530d60c
Martinello123 <45353166+Martinello123@users.noreply.github.com>
2018-12-17 08:40:44 +0100

    Update SecondDataProcessing.py

--- SecondDataProcessing.py
+++ SecondDataProcessing.py
@@ -1,8 +1,7 @@
-
-# Projekt Marcina
-
 import numpy as np
 from FileUtils import FileUtils
+import matplotlib.pyplot as plt
+
 
 file_name = 'data_from_spec_with_light.txt'
 
@@ -22,46 +21,57 @@ wave_lenghts = tmp.tolist()
 visible_wave_length = wave_lenghts[819:]
 visible_wave_length_intensity = data[819:]
 percent_wave_length_intensity = []
-# red
-funx = []
-# green
-funy = []
-# blue
-funz = []
-# funx = 1.065 * np.e((-1/2) * (((visible_wave_length)-595.8)/33.33) ^ 2)+0.366 * np.e((-1/2) * (((visible_wave_length)-446.8)/19.44) ^ 2)
-# funy(i) = 1.014.*exp((-1/2).*((log(i+399)-log(556.3))/0.075).^2);
-# funz(i) =  1.839.*exp((-1/2).*((log(i+399)-log(449.3))/0.051).^2);
 
+
+# red
+fun_x = []
+# green
+fun_y = []
+# blue
+fun_z = []
+
+# color matching functions
 for i in range(len(visible_wave_length_intensity)):
     percent_wave_length_intensity.append(visible_wave_length_intensity[i]/16383)
-    funx.append(1.065 * np.exp((-1/2) * ((visible_wave_length[i]-595.8)/33.33) ** 2)+0.366 * np.exp((-1/2) * ((visible_wave_length[i]-446.8)/19.44) ** 2))
-    funy.append(1.014*np.exp((-1/2)*((np.log(visible_wave_length[i])-np.log(556.3))/0.075)**2))
-    funz.append(1.839*np.exp((-1/2)*((np.log(visible_wave_length[i])-np.log(449.3))/0.051)**2))
+    fun_x.append(1.065 * np.exp((-1/2) * ((visible_wave_length[i]-595.8)/33.33) ** 2) + 0.366 * np.exp((-1/2) * ((visible_wave_length[i] - 446.8)/19.44) ** 2))
+    fun_y.append(1.014 * np.exp((-1/2) * ((np.log(visible_wave_length[i]) - np.log(556.3))/0.075) ** 2))
+    fun_z.append(1.839 * np.exp((-1/2) * ((np.log(visible_wave_length[i]) - np.log(449.3))/0.051) ** 2))
 
 # print(visible_wave_length)
 # print(percent_wave_length_intensity)
-# print(funz)
-x = 0
-y = 0
-z = 0
+
+X = 0
+Y = 0
+Z = 0
 
 for i in range(len(visible_wave_length_intensity)):
-    x = x + (percent_wave_length_intensity[i]*visible_wave_length[i]*10**(-9)*funx[i])
-    y = y + (percent_wave_length_intensity[i]*visible_wave_length[i]*10**(-9)*funy[i])
-    z = z + (percent_wave_length_intensity[i]*visible_wave_length[i]*10**(-9)*funz[i])
-
-print(x)
-print(y)
-print(z)
-
-xt = x/(x+y+z)
-yt = y/(x+y+z)
+    X = X + (percent_wave_length_intensity[i] * fun_x[i])
+    Y = Y + (percent_wave_length_intensity[i] * fun_y[i])
+    Z = Z + (percent_wave_length_intensity[i] * fun_z[i])
 
 
-print(xt)
-print(yt)
+xc = X/(X + Y + Z)
+yc = Y/(X + Y + Z)
 
-n = (xt-0.332)/(yt-0.1858)
-cct = 449*n**3+3525*n**2-6823.3*n+5520.33
+# CIE 1931 chromaticity coordinates
+print("Coordinate x:", xc)
+print("Coordinate y:", yc)
 
-print(cct)
+# CIE 1960 chromaticity coordinates
+u = 4 * xc / (-2 * xc + 12 * yc + 3)
+v = 6 * yc / (-2 * xc + 12 * yc + 3)
+
+print("\nCoordinate u:", u)
+print("Coordinate v:", v)
+
+# Calvin McCamy method
+n = (xc - 0.332)/(yc - 0.1858)
+cct = 449 * n ** 3 + 3525 * n ** 2 - 6823.3 * n + 5520.33
+
+print("\nColor temperature:", cct)
+
+plt.plot(wave_lenghts, data)
+plt.show()
+
+plt.plot(visible_wave_length, percent_wave_length_intensity, '.')
+plt.show()
